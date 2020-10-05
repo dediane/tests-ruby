c904f68ca1bd22b655c2b8758eaa4bfb2d238a8d
Diane Decourt <dianedecourt@gmail.com>
2020-10-05 17:44:39 +0200

    Update on 04_simon_says & 05_timer

--- lib/04_simon_says.rb
+++ lib/04_simon_says.rb
@@ -6,6 +6,18 @@ def shout(str)
   return str.upcase
 end
 
-def repeat(str, n)
-  
-  return 
+def repeat(str, *n)
+  return str + " " * (n -1) + str
+end
+
+def start_of_word(str, n)
+  return (str.slice(0...n))
+end
+
+def first_word(str)
+  return str.split.take(1) * ' ' 
+end
+
+def titleize(str)
+  return str.capitalize
+end
